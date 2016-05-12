f5514c3a9f2c6757e9e3033e4ff6859637d12092
Ivan Tuzhilkin <tiv57@hotmail.com>
2016-05-12 18:30:21 +0300

    add commentlines

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/Ivan-Tuzhilkin/angelina.git
+"""
+
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
@@ -7,9 +14,12 @@ setup(
     version = '1.0',
     description = 'The software for creating archives.',
     long_description = open(join(dirname(__file__), 'README.rst')).read(),
+	# Author details
     author = 'Ivan Tuzhilkin',
     author_email = 'tiv57@hotmail.com',
+	# Choose your license
     license = 'MIT',
+	# The project's main homepage.
     url = 'https://github.com/Ivan-Tuzhilkin/angelina.git',
     packages = find_packages(),
     entry_points = {
