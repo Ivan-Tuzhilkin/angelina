e8afd4f8ce9d0083a2dd1bf30f50f43b93eb1b7f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 18:30:08 +0000

    Adding tarfile member sanitization to extractall()

--- angelina/use_tar.py
+++ angelina/use_tar.py
@@ -150,7 +150,26 @@ def extract_tar(name_tarfile, new_dir):
     @type: str
     '''
     with tarfile.open(name_tarfile) as tar:
-        tar.extractall(new_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, new_dir)
 
 def change_tar(archive):
     '''
